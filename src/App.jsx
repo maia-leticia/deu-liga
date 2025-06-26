b6f4edb7851d75649733da1b32cb2efea8da1c4b
Leticia Maia <132717041+maia-leticia@users.noreply.github.com>
2025-06-26 18:32:43 -0300

    Organização background
    ajustei o background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header"
+import ElipseBackground from "./components/ElipseBackground"
 
 function App() {
   return (
     <>
-        <main className="min-h-screen bg-[#880808]">
-          <Header/>
+        <main className="relative bg-[#880808]">
+            <Header />
+            <ElipseBackground className="absolute -translate-x-[20vw] -translate-y-[25vw]"/>
+            <div className="relative z-20 px-[70px]">
+              <h1 className="text-[50px]" >INGREDIENTES</h1>
+            </div>
         </main>
     </>
   )
